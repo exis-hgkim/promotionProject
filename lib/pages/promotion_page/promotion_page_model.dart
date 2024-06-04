dea9956e60634a75676b543ceb16365cfb146a40
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-04 06:44:18 +0000

    Promotion 목록 화면 레이아웃 수정 (2024-06-04)

--- lib/pages/promotion_page/promotion_page_model.dart
+++ lib/pages/promotion_page/promotion_page_model.dart
@@ -1,3 +1,4 @@
+import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/form_field_controller.dart';
 import 'promotion_page_widget.dart' show PromotionPageWidget;
@@ -10,56 +11,40 @@ class PromotionPageModel extends FlutterFlowModel<PromotionPageWidget> {
 
   ///  State fields for stateful widgets in this page.
 
-  // State field(s) for RadioButton widget.
-  FormFieldController<String>? radioButtonValueController1;
-  DateTime? datePicked1;
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode1;
   TextEditingController? textController1;
   String? Function(BuildContext, String?)? textController1Validator;
-  DateTime? datePicked2;
+  // State field(s) for RadioButton widget.
+  FormFieldController<String>? radioButtonValueController1;
+  DateTime? datePicked1;
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode2;
   TextEditingController? textController2;
   String? Function(BuildContext, String?)? textController2Validator;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue1;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue2;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue3;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue4;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue5;
-  // State field(s) for RadioButton widget.
-  FormFieldController<String>? radioButtonValueController2;
+  DateTime? datePicked2;
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode3;
   TextEditingController? textController3;
   String? Function(BuildContext, String?)? textController3Validator;
+  // State field(s) for CheckboxGroup widget.
+  FormFieldController<List<String>>? checkboxGroupValueController;
+
+  List<String>? get checkboxGroupValues => checkboxGroupValueController?.value;
+  set checkboxGroupValues(List<String>? v) =>
+      checkboxGroupValueController?.value = v;
+
+  // State field(s) for RadioButton widget.
+  FormFieldController<String>? radioButtonValueController2;
   // State field(s) for Checkbox widget.
-  bool? checkboxValue6;
+  bool? checkboxValue1;
   // State field(s) for Checkbox widget.
-  bool? checkboxValue7;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue8;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue9;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue10;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue11;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue12;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue13;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue14;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue15;
-  // State field(s) for Checkbox widget.
-  bool? checkboxValue16;
+
+  Map<PromotionsRecord, bool> checkboxValueMap2 = {};
+  List<PromotionsRecord> get checkboxCheckedItems2 => checkboxValueMap2.entries
+      .where((e) => e.value)
+      .map((e) => e.key)
+      .toList();
 
   @override
   void initState(BuildContext context) {}
